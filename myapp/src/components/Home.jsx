6a657d1932b5d5d7caab4bc717f5932baa3fe682
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-06-04 11:33:00 +0530

    Link update

--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -12,11 +13,13 @@ const Home = () => {
               <p className=' text-[#8892b0] py-4 max-w-[700px] '>I'm a Full-Stack Developer specializing in building and designing exceptional digital experience. I am interested in creating well designed or pixel-perfect Frontend with functionality and take on new 
                             challenges where I can learn more about web technologies.</p>
                <div>
+                 <Link  to="work" smooth={true}  duration={500} >
                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 '>View Work 
                    <span className='group-hover:rotate-90 duration-300 '>
                        <HiArrowNarrowRight className='ml-3 '/>
                    </span>
                    </button>
+                   </Link> 
                  
                </div>
            </div>
